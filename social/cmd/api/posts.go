67d4a56d630628948c77625cbb81b5b7a1ad785d
Sergey Zarochentsev <sergey.zrch@gmail.com>
2025-01-03 16:17:34 +0500

    HTTP Payload Validation

--- social/cmd/api/posts.go
+++ social/cmd/api/posts.go
@@ -9,8 +9,8 @@ import (
 )
 
 type CreatePostPayload struct {
-	Title   string   `json:"title"`
-	Content string   `json:"content"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
 
@@ -18,6 +18,11 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	var payload CreatePostPayload
 
 	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
 		app.badRequestError(w, r, err)
 		return
 	}
